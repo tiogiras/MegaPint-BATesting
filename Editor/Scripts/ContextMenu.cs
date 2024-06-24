f6d5125e3782e2164d495e8d40831e5f9b049bc6
Tiogiras <tiogiras@gmail.com>
2024-06-24 14:17:30 +0200

    First Window script

--- Editor/Scripts/ContextMenu.cs
+++ Editor/Scripts/ContextMenu.cs
@@ -1,10 +1,22 @@
 ﻿#if UNITY_EDITOR
+using MegaPint.com.tiogiras.megapint_batesting.Editor.Scripts.Windows;
+using UnityEditor;
+
 namespace MegaPint.Editor.Scripts
 {
 
 /// <summary> Partial class used to store MenuItems </summary>
 internal static partial class ContextMenu
 {
+    #region Private Methods
+
+    [MenuItem(MenuItemPackages + "/BA Testing" + "/Task Manager", false, 100)]
+    private static void OpenTaskManager()
+    {
+        TryOpen <TaskManager>(false);
+    }
+
+    #endregion
 }
 
 }
