b874d9e19b2ea5c33d986705c7fe019875cb62b5
Tiogiras <tiogiras@gmail.com>
2024-07-01 12:05:41 +0200

    QuickSave

--- Editor/Scripts/Windows/TaskManager.cs
+++ Editor/Scripts/Windows/TaskManager.cs
@@ -1,4 +1,8 @@
-﻿#if UNITY_EDITOR
+﻿// TODO commenting
+
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using MegaPint.com.tiogiras.megapint_batesting.Editor.Scripts.Windows.TaskManagerContent;
 using MegaPint.Editor.Scripts;
 using MegaPint.Editor.Scripts.GUI.Utility;
 using MegaPint.Editor.Scripts.Windows;
@@ -14,6 +18,20 @@ internal class TaskManager : EditorWindowBase
 {
     private VisualTreeAsset _baseWindow;
     private VisualTreeAsset _requirementItem;
+
+    private TaskManagerData _data;
+
+    private Label _lastTaskIndex;
+    private Label _currentTaskIndex;
+    private Label _taskTitle;
+    private Label _taskInfo;
+    private Label _chapter;
+    
+    private VisualElement _requirementsContainer;
+    private ListView _requirementsList;
+
+    private Button _btnContinue;
+    private Button _btnStart;
 
     #region Public Methods
 
@@ -53,7 +71,39 @@ internal class TaskManager : EditorWindowBase
         content.style.flexGrow = 1f;
         content.style.flexShrink = 1f;
 
+        _lastTaskIndex = content.Q <Label>("LastTaskIndex");
+        _currentTaskIndex = content.Q <Label>("CurrentTaskIndex");
+        _taskTitle = content.Q <Label>("TaskTitle");
+        _taskInfo = content.Q <Label>("TaskInfo");
+        _chapter = content.Q <Label>("Chapter");
+        
+        _requirementsContainer = content.Q <VisualElement>("RequirementsParent");
+        _requirementsList = content.Q <ListView>("Requirements");
+
+        _btnContinue = content.Q <Button>("BTN_Continue");
+        _btnStart = content.Q <Button>("BTN_Start");
+
+        UpdateTaskManager();
+
         RegisterCallbacks();
+    }
+
+    private void UpdateTaskManager()
+    {
+        _lastTaskIndex.text = $"/{_data.TasksCount}";
+        _currentTaskIndex.text = (_data.currentTaskIndex + 1).ToString();
+
+        Task currentTask = _data.CurrentTask();
+        _taskTitle.text = currentTask.taskName;
+        _taskInfo.text = currentTask.taskDescription;
+
+        _chapter.text = currentTask.chapter.chapterName;
+
+        var hasRequirements = currentTask.taskRequirements.Count > 0;
+        _requirementsContainer.style.display = hasRequirements ? DisplayStyle.Flex : DisplayStyle.None;
+
+        _btnContinue.style.display = currentTask.hasDoableTask ? DisplayStyle.None : DisplayStyle.Flex;
+        _btnStart.style.display = currentTask.hasDoableTask ? DisplayStyle.Flex : DisplayStyle.None;
     }
 
     protected override bool LoadResources()
@@ -61,17 +111,26 @@ internal class TaskManager : EditorWindowBase
         _baseWindow = Resources.Load <VisualTreeAsset>(BasePath());
         _requirementItem = Resources.Load <VisualTreeAsset>(Constants.BaTesting.UserInterface.Requirement);
 
-        return _baseWindow != null && _requirementItem != null;
+        _data = Resources.Load <TaskManagerData>(Constants.BaTesting.TaskManagerData);
+
+        return _baseWindow != null && _requirementItem != null && _data != null;
     }
 
     protected override void RegisterCallbacks()
     {
-
+        _btnContinue.clicked += OnContinue;
     }
 
     protected override void UnRegisterCallbacks()
     {
+        _btnContinue.clicked -= OnContinue;
+    }
 
+    private void OnContinue()
+    {
+        _data.currentTaskIndex++;
+        
+        UpdateTaskManager();
     }
 
     #endregion
---
+++ Editor/Scripts/Windows/TaskManagerContent/Chapter.cs
@@ -0,0 +1,14 @@
+// TODO Commenting
+
+using UnityEngine;
+
+namespace MegaPint.Editor.Scripts.Windows.TaskManagerContent
+{
+
+[CreateAssetMenu(fileName = "Chapter", menuName = "MegaPint/Chapter", order = 0)] // TODO remove this line
+internal class Chapter : ScriptableObject
+{
+    public string chapterName;
+}
+
+}
